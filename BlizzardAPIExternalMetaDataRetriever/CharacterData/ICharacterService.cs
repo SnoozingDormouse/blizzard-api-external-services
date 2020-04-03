9069d6c9016597547d98c065d06c75b8bc48108e
Dorothy Hawley <snoozing.dormouse@gmail.com>
2020-04-03 15:46:34 +0100

    controller framework and updated character table to include last updated timestamp

---
+++ BlizzardAPIExternalMetaDataRetriever/CharacterData/ICharacterService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlizzardAPIExternalMetaDataRetriever.CharacterData
+{
+    public interface ICharacterService
+    {
+        void UpdateCharacterData(string region, string realm, string name);
+    }
+}
---
+++ BlizzardAPIExternalMetaDataRetriever/Controllers/CharacterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlizzardAPIExternalMetaDataRetriever.Controllers
+{
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        // GET: api/Character/{Region}/{Realm}/{Character}
+        [HttpGet("{region}/{realm}/{character}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<string> UpdateCharacter(string region, string realm, string character)
+        {
+            throw new NotImplementedException();
+            // invoke pull from external services
+            //return await _characterService.Update(region, realm, character);
+        }
+    }
+}
